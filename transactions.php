0843b6019bbc7914a44951572eab11feb479193a
chandru_train <sraj.chandru@gmail.com>
2020-08-24 22:06:28 +0530

    fifth commit

--- transactions.php
+++ transactions.php
@@ -17,7 +17,7 @@ if(isset($_POST['search']))
 
 function filterTable($query)
 {
-    $connect = mysqli_connect("db", "admin", "admin123", "etldev");
+    $connect = mysqli_connect("dbname", "admin", "admin123", "etldev");
     $filter_Result = mysqli_query($connect, $query);
     return $filter_Result;
 }
